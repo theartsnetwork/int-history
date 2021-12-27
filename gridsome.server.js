531cf5e3b1b51ec2612ac68a9fa6e9e5a93c9e6f
The Arts Network <7610386+theartsnetwork@users.noreply.github.com>
2021-12-27 15:26:38 +0530

    marcel-update

--- gridsome.server.js
+++ gridsome.server.js
@@ -35,22 +35,15 @@ class SPARQLQueryDispatcher {
 }
 
 const endpointUrl = "https://query.wikidata.org/sparql";
-const sparqlQuery = `SELECT DISTINCT ?painting (MIN(?images) AS ?image) (MIN(?dates) AS ?date) (MIN(?locationLabels) AS ?locationLabel) (GROUP_CONCAT(DISTINCT ?materialLabel; SEPARATOR = ", ") AS ?materials) (GROUP_CONCAT(DISTINCT ?depictLabel; SEPARATOR = ", ") AS ?depicts) WHERE {
-  ?painting (wdt:P31/(wdt:P279*)) wd:Q3305213;
-    wdt:P18 ?images;
-    wdt:P571 ?dates.
-  OPTIONAL { ?painting wdt:P276 ?locations. }
-  OPTIONAL { ?painting wdt:P186 ?material. }
-  OPTIONAL { ?painting wdt:P180 ?depict. }
-  SERVICE wikibase:label {
-    bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en".
-    ?painting rdfs:label ?paintingLabel.
-    ?locations rdfs:label ?locationLabels.
-    ?material rdfs:label ?materialLabel.
-    ?depict rdfs:label ?depictLabel.
+const sparqlQuery = `SELECT DISTINCT ?item ?itemLabel WHERE {
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE]". }
+  {
+    SELECT DISTINCT ?item WHERE {
+      ?item p:P170 ?statement0.
+      ?statement0 (ps:P170/(wdt:P279*)) wd:Q5912.
+    }
+    LIMIT 200
   }
-  
-  ?painting wdt:P170 wd:Q5432.
 }
 GROUP BY ?painting ?paintingLabel ?image ?date ?locationLabel ?materials ?depicts
 ORDER BY (?date)`;
